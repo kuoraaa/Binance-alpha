12ccc363812704a4b203a3881e0256c7970042c9
kuoraaa <kuoraaa@gmail.com>
2025-10-12 21:37:34 +0700

    Test 5
    ganti lagi main.apy

--- main.py
+++ main.py
@@ -2,31 +2,24 @@ import os
 import discord
 from discord.ext import commands
 
-# Ambil token dari environment variable (Railway)
-TOKEN = os.getenv("MTQyNjkyNzgzNjIwMzE4ODM5Nw.Gookgp.DYIDlCA4EmJxtPOpIcDs161moKg90ukzx2DCd0")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Cek apakah token ditemukan
 if TOKEN is None:
     raise ValueError("❌ DISCORD_TOKEN tidak ditemukan! Pastikan sudah diatur di Railway Variables.")
 
-# Mengatur intents (izin bot)
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
-intents.message_content = True  # penting untuk bisa membaca isi pesan
+intents.message_content = True
 
-# Membuat instance bot dengan prefix '!'
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Event ketika bot siap digunakan
 @bot.event
 async def on_ready():
     print(f"✅ Bot berhasil login sebagai {bot.user}")
 
-# Contoh command sederhana: !hello
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Halo {ctx.author.mention}! 👋 Aku sudah online!")
 
-# Jalankan bot
 bot.run(TOKEN)
